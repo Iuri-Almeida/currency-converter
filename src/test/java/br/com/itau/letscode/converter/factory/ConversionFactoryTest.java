55db697eab4651c7bb3230d14bdf12810ccdcca8
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-08-21 17:18:35 -0300

    adding ConversionFacroryTest

---
+++ src/test/java/br/com/itau/letscode/converter/factory/ConversionFactoryTest.java
@@ -0,0 +1,39 @@
+package br.com.itau.letscode.converter.factory;
+
+import br.com.itau.letscode.converter.impl.ConversionWithValidation;
+import br.com.itau.letscode.currency.*;
+import org.junit.jupiter.api.*;
+
+class ConversionFactoryTest {
+
+    static ConversionFactory conversionFactory;
+
+    @BeforeAll
+    static void setUp() {
+        conversionFactory = new ConversionFactory();
+    }
+
+    @Test
+    void create() {
+        ConversionWithValidation dollar = conversionFactory.create(1);
+        ConversionWithValidation euro = conversionFactory.create(2);
+        ConversionWithValidation argentineanPeso = conversionFactory.create(3);
+        ConversionWithValidation chileanPeso = conversionFactory.create(4);
+        ConversionWithValidation poundSterling = conversionFactory.create(5);
+        ConversionWithValidation yen = conversionFactory.create(6);
+
+        Assertions.assertEquals(dollar.getClass(), Dollar.class);
+        Assertions.assertEquals(euro.getClass(), Euro.class);
+        Assertions.assertEquals(argentineanPeso.getClass(), ArgentinianPeso.class);
+        Assertions.assertEquals(chileanPeso.getClass(), ChileanPeso.class);
+        Assertions.assertEquals(poundSterling.getClass(), PoundSterling.class);
+        Assertions.assertEquals(yen.getClass(), Yen.class);
+    }
+
+    @Test
+    void shouldThrownExceptionWhenInvalidKey() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            conversionFactory.create(7);
+        });
+    }
+}
